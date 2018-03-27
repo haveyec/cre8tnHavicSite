eb5dcdb72e557e46484e3b6073cabb99e7fcd757
Havic <haveyec@me.com>
2018-03-27 15:16:40 -0400

    Homepage
    Code all in, just having a little issue...don't know why.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :pages
+  resources :pages, :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root "pages#index", page: "home"
   root "pages#home"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327173925) do
+ActiveRecord::Schema.define(version: 20180327191138) do
+
+  create_table "homes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "title"
