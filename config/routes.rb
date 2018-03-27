1da05da27a6db6d91a59343af6488b0d42e5db26
Havic <haveyec@me.com>
2018-03-27 15:49:28 -0400

    remove homes from route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :pages, :home
+  resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root "pages#index", page: "home"
   root "pages#home"
